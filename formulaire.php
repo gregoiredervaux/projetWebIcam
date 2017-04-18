d64d217dca5cb66446da9cb3b864b170fc623de1
Guillaume Dubois <dbmgui@gmail.com>
2017-04-18 23:09:08 +0200

    update head plus debut formulaire

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,26 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php include("head.php"); ?>
+		<?php include("inclusion/head.php"); ?>
 	</head>
-</html>
+
+	<div class="input-group" id="mail">
+  		<span class="input-group-addon" id="sizing-addon2">@</span>
+  		<input type="text" class="form-control" placeholder="Adresse email" aria-describedby="sizing-addon2">
+	</div>
+
+	<div class="input-group" id="nom">
+  		<span class="input-group-addon" id="sizing-addon2"></span>
+  		<input type="text" class="form-control" placeholder="Nom" aria-describedby="sizing-addon2">
+	</div>
+
+	<div class="input-group" id='prenom'>
+  		<span class="input-group-addon" id="sizing-addon2"></span>
+  		<input type="text" class="form-control" placeholder="Prenom" aria-describedby="sizing-addon2">
+	</div>
+
+	<div class="input-group" id='telephone'>
+  		<span class="input-group-addon" id="sizing-addon2" label=@glyphicon glyphicon-earphone></span>
+  		<input type="text" class="form-control" placeholder="Telephone" aria-describedby="sizing-addon2">
+	</div>
+</html> 	
--- inclusion/head.php
+++ inclusion/head.php
@@ -1,1 +1,11 @@
-head.php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Gala Icam</title>
+	</head>
+</html>
