1e7a4b7354661cfe52c6b6b80b043ea26c9c2ac4
Guillaume Dubois <dbmgui@gmail.com>
2017-04-18 23:34:04 +0200

    maj formulaire

--- formulaire.php
+++ formulaire.php
@@ -3,24 +3,25 @@
 	<head>
 		<?php include("inclusion/head.php"); ?>
 	</head>
-
-	<div class="input-group" id="mail">
-  		<span class="input-group-addon" id="sizing-addon2">@</span>
-  		<input type="text" class="form-control" placeholder="Adresse email" aria-describedby="sizing-addon2">
-	</div>
-
-	<div class="input-group" id="nom">
-  		<span class="input-group-addon" id="sizing-addon2"></span>
-  		<input type="text" class="form-control" placeholder="Nom" aria-describedby="sizing-addon2">
-	</div>
-
-	<div class="input-group" id='prenom'>
-  		<span class="input-group-addon" id="sizing-addon2"></span>
-  		<input type="text" class="form-control" placeholder="Prenom" aria-describedby="sizing-addon2">
-	</div>
-
-	<div class="input-group" id='telephone'>
-  		<span class="input-group-addon" id="sizing-addon2" label=@glyphicon glyphicon-earphone></span>
-  		<input type="text" class="form-control" placeholder="Telephone" aria-describedby="sizing-addon2">
-	</div>
+	<nav> <!-- input formulaire invite -->
+		<div class="input-group" id="nom">
+  		<span class="input-group-addon" id="sizing-addon2">Nom</span>
+		<input type="text" class="form-control" aria-describedby="sizing-addon2">
+		</div>
+		<br>
+		<div class="input-group" id='prenom'>
+	  		<span class="input-group-addon" id="sizing-addon2">Prénom</span>
+	  		<input type="text" class="form-control" aria-describedby="sizing-addon2">
+		</div>
+		<br>
+		<div class="input-group" id="mail">
+	  		<span class="input-group-addon" id="sizing-addon2">@</span>
+	  		<input type="text" class="form-control" placeholder="Adresse email" aria-describedby="sizing-addon2">
+		</div>
+		<br>
+		<div class="input-group" id='telephone'>
+	  		<span class="input-group-addon" id="sizing-addon2"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></span>
+	  		<input type="text" class="form-control" placeholder="Telephone" aria-describedby="sizing-addon2">
+		</div>
+	</nav>
 </html> 	
