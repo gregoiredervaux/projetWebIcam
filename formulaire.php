5f87ad26cbec230ce3538c979ca4823dabfa97f5
Guillaume Dubois <dbmgui@gmail.com>
2017-04-19 16:30:12 +0200

    Modif entête plus ajutement formulaire

--- formulaire.php
+++ formulaire.php
@@ -2,8 +2,13 @@
 <html>
 	<head>
 		<?php include("inclusion/head.php"); ?>
+		<?php include("inclusion/entete.php"); ?>
 	</head>
 	<nav> <!-- input formulaire invite -->
+		<br>
+		<br>
+		<br>
+		
 		<div class="input-group" id="nom">
   		<span class="input-group-addon" id="sizing-addon2">Nom</span>
 		<input type="text" class="form-control" aria-describedby="sizing-addon2">
---
+++ inclusion/entete.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- Le styles -->
+    <!-- <link href="projetweb/css/bootstrap.min.css" rel="stylesheet"> -->
+<!--     <link href="projetweb/css/main.css" rel="stylesheet">
+ --><!--     <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+ -->  </head>
+    <body>
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header"><!-- 
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button> -->
+          <a class="navbar-brand" href="localhost/projetweb/">Gala Icam</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Accueil</a></li> <!-- penser a remplacer li par <li class=active> pour onglet en cours -->
+            <li><a href="localhost/projetweb/formulaire/">Formulaire</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </body>
+  </html>
